4d9de58a68bba10daac25426cc4efe53c4bc16b5
wangxue <wangxue@yongche.com>
2019-01-03 17:33:55 +0800

    bug fix

--- app/src/main/java/com/wangxue/wxlog/MainActivity.java
+++ app/src/main/java/com/wangxue/wxlog/MainActivity.java
@@ -32,14 +32,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void uploadLog() {
-        HashMap<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "binary/octet-stream"); //二进制上传
-        headers.put("client", "android");
-        new RealSendLogRunnable.SendLogBuilder()
-                .setHeaders(headers)
-                .setMethod(RealSendLogRunnable.SendLogBuilder.POST)
-                .setUrl("http://192.168.1.7:3000/logupload")
-                .build()
-                .doIT();
+        if(BuildConfig.ENABLE_LOG_TEST || !BuildConfig.PLANT_DEBUG) {
+            String url = "http://192.168.119.63:8080/test/logupload";//换成自己的url
+            HashMap<String, String> headers = new HashMap<>();
+            headers.put("Content-Type", "application/octet-stream"); //二进制流
+            headers.put("client", "android");
+            new RealSendLogRunnable.SendLogBuilder()
+                    .setHeaders(headers)
+                    .setMethod(RealSendLogRunnable.SendLogBuilder.POST)
+                    .setUrl(url)
+                    .build()
+                    .doIT();
+        }
     }
 }
